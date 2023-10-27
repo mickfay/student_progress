2faecee9b5eaebce846489d5d057653b5a2e9d6e
mickfay <mick.fay@hotmail.com>
2023-10-27 16:51:14 +0100

    This commit will add functionality to extract tasks completed from a passed filepath with testing. One big commit for everything, terrible practice I know

--- __tests__/student_progress.test.js
+++ __tests__/student_progress.test.js
@@ -1,11 +1,99 @@
-const {readFile, progressTracker} = require('../student_progress')
+const {progressTracker, progressTrackerArr, extractNameAndTasks} = require('../student_progress')
+const fs = require('fs')
 
-describe('progressTracker', () => {
-    
-});
+const mockData = `X X X X                                      Student One
+X X X X X X X                                Student Two
+                                             Student Three
+                                             X X X                                        Student Four
+                                             X X X X                                      Student Five
+                                             X X X X X X X X                              Student Six`
+                                             
+const spyFsReadFileSync = jest.spyOn(fs,'readFileSync')
 
-describe('readFile', () => {
-    test('Function should read the data in a file', () => {
-        expect(readFile('../test.txt')).toBe('This is a test file')
+describe('progressTrackerArr', () => {
+    // Test of function mocking
+    test('Function returns an array of one student when passed a textfile of one student', () => {
+        console.log(fs)
+        spyFsReadFileSync.mockReturnValue('X X X               Michael Fay')
+        //const mockData = jest.fn(() => {return 'X X X               Michael Fay'})
+        expect(progressTrackerArr('test.txt')).toEqual(['Task 3 - Michael Fay'])     
+    });
+
+    test('Function returns an array of two students when passed a file of multiple students with correct task numbers', () => {
+        const mockData = 'X X X               Michael Fay\nX X X X X X            Daniel Craven'
+        spyFsReadFileSync.mockReturnValue(mockData)
+        const expectedOutput = ['Task 3 - Michael Fay', 'Task 6 - Daniel Craven']
+        expect(progressTrackerArr('test.txt')).toEqual(expectedOutput)
+    });
+    test('Function correctly identifies a student with 0 tasks completed', () => {
+        const mockData = '               Michael Fay\nX X X             Daniel Craven\nX X X X X X             Michael Tucker'
+        spyFsReadFileSync.mockReturnValue(mockData)
+
+        const expectedOutput = ['Task 0 - Michael Fay', 'Task 3 - Daniel Craven', 'Task 6 - Michael Tucker']
+        expect(progressTrackerArr('test.txt')).toEqual(expectedOutput)
+    });
+    test('Function should not identify if a name has an "X " in, and ignore when calculating task number', () => {
+        const mockData = 'X X X               Michael X Fay\nX X X X X X            Daniel Craven'
+        spyFsReadFileSync.mockReturnValue(mockData)
+        const expectedOutput = ['Task 3 - Michael X Fay', 'Task 6 - Daniel Craven']
+        expect(progressTrackerArr('test.txt')).toEqual(expectedOutput)
+    });
+    test('Function should sort students in progress order', () => {
+        const mockData = 'X X X             Michael Fay\n           Daniel Craven\nX X X X X X          Edward Norton\nX                Greta Hunberston'
+        spyFsReadFileSync.mockReturnValue(mockData)
+        const expectedOutput = ['Task 0 - Daniel Craven', 'Task 1 - Greta Hunberston', 'Task 3 - Michael Fay', 'Task 6 - Edward Norton']
+        expect(progressTrackerArr('test.txt')).toEqual(expectedOutput)
+    });
+    test('Function should return correct values for tasks completed over 10', () => {
+        const mockData = 'X X X X X X X X X X X X            Michael Fay\nX X X           Daniel Crave'
+        spyFsReadFileSync.mockReturnValue(mockData)
+        const expectedOutput = ['Task 12 - Michael Fay', 'Task 3 - Daniel Craven']
+ 
     });
 });
+
+describe('extractName', () => {
+    test('Function should extract a name and tasks from a string containing Xes representing tasks', () => {
+        const input = 'X X X X X X                      Michael Fay'
+        const expectedOutput = {name : 'Michael Fay', tasks : 6}
+        expect(extractNameAndTasks(input)).toEqual(expectedOutput)
+    });
+    test('Function should ignore Xes in the middle of names', () => {
+        const input = 'X X X X                  Xavier X Xtreme'
+        const expectedOutput = {name : 'Xavier X Xtreme', tasks : 4}
+        expect(extractNameAndTasks(input)).toEqual(expectedOutput)
+    })
+    test('Function should return an object with 0 tasks if 0 tasks completed', () => {
+        const input = '                 Michael X Fay'
+        const expectedOutput = {name : 'Michael X Fay', tasks : 0}
+    });
+});
+
+describe('progressTracker', () => {
+    const RealDate = Date.now
+    beforeAll(() => {
+        global.Date.now = jest.fn(() => new Date('2023-27-10T10:20:30Z').getTime())
+      })
+      
+      afterAll(() => {
+        global.Date.now = RealDate
+      })
+    test('Function should return a string of the filename that has been created', () => {
+        const mockData = '               Michael Fay\nX X X             Daniel Craven\nX X X X X X             Michael Tucker'
+        spyFsReadFileSync.mockReturnValue(mockData)
+        const result = progressTracker('example.txt')
+        const expected = 'File "Student_Progress_27.10.2023.txt" saved!'
+        expect(typeof result).toBe('string')
+        expect(result).toBe(expected)
+    });
+    test('Function should write data to a new file, named by date', () => {
+        const mockData = '               Michael Fay\nX X X             Daniel Craven\nX X X X X X             Michael Tucker'
+        spyFsReadFileSync.mockReturnValue(mockData)
+        progressTracker('example.txt')
+        jest.restoreAllMocks()
+        const writtenFile = fs.readFileSync('Student_Progress_27.10.2023.txt', 'utf8')
+        const expectedOutput = 'Task 0 - Michael Fay\nTask 3 - Daniel Craven\nTask 6 - Michael Tucker'
+        expect(writtenFile).toBe(expectedOutput)
+        
+    });
+})
--- student_progress.js
+++ student_progress.js
@@ -1,14 +1,52 @@
 const fs = require('fs')
 
-function progressTracker(trackFilePath) {
+function progressTracker(trackFilePath){
+    const progressArray = progressTrackerArr(trackFilePath)
+    const returnString = progressArray.join('\n')
+    const date = new Date()
 
+    const fileName = `Student_Progress_${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}.txt`
+
+    fs.writeFileSync(fileName, returnString)
+
+    return `File "${fileName}" saved!`
 }
 
-function readFile(filePath){
-    fs.readFile('../test.txt', 'utf8', (err, data) => {
-        console.log(data)
+function progressTrackerArr(trackFilePath) {
+    const data = fs.readFileSync(trackFilePath, 'utf8')
+    const studentDataArray = data.split('\n')
+    const studentArr = studentDataArray.map((element) => {
+        return extractNameAndTasks(element)
+
     })
+    const sortedArr = studentArr.sort((person1, person2) => {
+        return person1.tasks - person2.tasks
+    })
+    const stringedArr = sortedArr.map((studentObj)=>{
+        return `Task ${studentObj.tasks} - ${studentObj.name}`
+
+    })
+    return stringedArr
 }
 
+function extractNameAndTasks(string){
+    const splitString = string.split(' ')
+    const wordsAndTasks = splitString.filter((element) => {return element !== ''})
+    let tasksOnly = true
+    return wordsAndTasks.reduce((outcome, element) => {
+        if (element === 'X' && tasksOnly === true){
+            outcome.tasks++
+            return outcome
+        } else if(element !== 'X' && tasksOnly === true){
+            tasksOnly = false
+            outcome.name = element
+            return outcome
+        }
+        else if (tasksOnly === false){
+            outcome.name = outcome.name + ' ' + element
+            return outcome
+        }
+    },{tasks : 0})
+}
 
-module.exports = {readFile, progressTracker}
+module.exports = {progressTracker, progressTrackerArr, extractNameAndTasks}
